79be92444eb7ec0d935cee8110f2b4124056a5b5
Mhammad Osama Farooqui <osamafarooqui0987@gmail.com>
2023-05-29 19:55:55 +0500

    First commit before working on this project

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:book_reading_app/Views/Splash_Screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}

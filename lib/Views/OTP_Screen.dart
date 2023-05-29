39216c1af1d371bf74aaad39af1d5ebd905d0baa
Mhammad Osama Farooqui <osamafarooqui0987@gmail.com>
2023-05-30 02:34:32 +0500

    OTP screen completed.

--- lib/Views/OTP_Screen.dart
+++ lib/Views/OTP_Screen.dart
@@ -1,4 +1,7 @@
+import 'package:book_reading_app/Views/Custom_opt.dart';
+import 'package:book_reading_app/Views/MainScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class OtpScreen extends StatefulWidget {
   const OtpScreen({super.key});
@@ -8,10 +11,160 @@ class OtpScreen extends StatefulWidget {
 }
 
 class _OtpScreenState extends State<OtpScreen> {
+  TextEditingController FirstOPt = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        leading: const Icon(Icons.arrow_back),
+        title: const Text('Verification Screen'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image(
+                height: 300,
+                width: 300,
+                image: AssetImage('assets/images/auth.png'),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Column(
+                children: [
+                  Text(
+                    'OPT VERIFICATION',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    // crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Enter the OTP sent to',
+                        style: TextStyle(
+                          color: Colors.black,
+                          // fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Text(
+                        ' +92 315-1234567',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CustomOpt(customcontroller: FirstOPt),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      CustomOpt(customcontroller: FirstOPt),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      CustomOpt(customcontroller: FirstOPt),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      CustomOpt(customcontroller: FirstOPt),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    '30 Sec',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't receive code? ",
+                        style: TextStyle(
+                          color: Colors.black,
+                          // fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Text(
+                        "Re-Send ",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      if (FirstOPt.text.toString() == "2") {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => MainScreen(),
+                          ),
+                        );
+                      } else {
+                        print('Error');
+                      }
+                    },
+                    child: Container(
+                      height: 50,
+                      decoration: BoxDecoration(color: Colors.blue.shade700),
+                      child: Center(
+                        child: Text(
+                          'Submit',
+                          style: TextStyle(
+                            fontSize: 23,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
